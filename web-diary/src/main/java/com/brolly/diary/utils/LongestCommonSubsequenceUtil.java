ffa878a4b0b2b3c79c75e0b20c4b32e0b494a42e
banepesic <postarsmith@gmail.com>
2017-09-07 18:53:48 +0200

    refactoring util LCS class

--- web-diary/src/main/java/com/brolly/diary/utils/LongestCommonSubsequenceUtil.java
+++ web-diary/src/main/java/com/brolly/diary/utils/LongestCommonSubsequenceUtil.java
@@ -1,20 +1,17 @@
 package com.brolly.diary.utils;
 
-import java.io.*;
 
-class  LongestCommonSubsequence2
+class  LongestCommonSubsequenceUtil
 {
     // Returns length of LCS for X[0..m-1], Y[0..n-1]
-    static void lcs(String X, String Y, int m, int n)
-    {
-        int[][] L = new int[m+1][n+1];
+    public static char[] lcs(String X, String Y, int m, int n) {
+        
+    	int[][] L = new int[m+1][n+1];
   
         // Following steps build L[m+1][n+1] in bottom up fashion. Note
         // that L[i][j] contains length of LCS of X[0..i-1] and Y[0..j-1] 
-        for (int i=0; i<=m; i++)
-        {
-            for (int j=0; j<=n; j++)
-            {
+        for (int i=0; i<=m; i++) {
+            for (int j=0; j<=n; j++) {
                 if (i == 0 || j == 0)
                     L[i][j] = 0;
                 else if (X.charAt(i-1) == Y.charAt(j-1))
@@ -26,7 +23,6 @@ class  LongestCommonSubsequence2
   
         // Following code is used to print LCS
         int index = L[m][n];
-        int temp = index;
   
         // Create a character array to store the lcs string
         char[] lcs = new char[index+1];
@@ -57,22 +53,26 @@ class  LongestCommonSubsequence2
             else
                 j--;
         }
-  
-        // Print the lcs
-        System.out.print("LCS of "+X+" and "+Y+" is ");
-        for(int k=0;k<=temp;k++)
-            System.out.print(lcs[k]);
+        
+        return lcs;
+       
     }
      
-    // driver program
+    
     public static void main (String[] args) 
     {
         String X = "AGGTAB";
         String Y = "GXTXAYB";
         int m = X.length();
         int n = Y.length();
-        lcs(X, Y, m, n);
+        
+        char[] lcs = lcs(X, Y, m, n);
+        
+        // Print the lcs
+        System.out.print("LCS of " + X +" and "+ Y +" is ");
+        for(char c: lcs){
+        	System.out.print(c);
+        }
     }
 }
  
-// Contributed by Pramod Kumar
